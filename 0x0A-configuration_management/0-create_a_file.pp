11b0baab1c918000a39011f7d511f19b913695f3
nickuchida <nicholas.y.uchida@gmail.com>
2020-03-23 19:48:24 +0000

    first task and readme

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Create a file with Puppet
+
+file {
+  '/tmp/holberton':
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,3 @@
+## 0x0A Configuration Management
+
+Configuration management project using puppet-lint.
